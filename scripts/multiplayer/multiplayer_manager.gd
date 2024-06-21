701007812a13984a0d3cd2c0e4f22139cb536036
Subhendu  Karmakar <subhendukarmakar61@gmail.com>
2024-06-21 15:50:44 +0530

    client and server for multiplayer game is created

--- scripts/multiplayer/multiplayer_manager.gd
+++ scripts/multiplayer/multiplayer_manager.gd
@@ -1,8 +1,30 @@
 extends Node
 
+var SERVER_PORT = 8080
+var SERVER_IP = "127.0.0.1"
+
 func become_host():
 	print("Become host pressed")
 	
+	var server_peer = ENetMultiplayerPeer.new()
+	server_peer.create_server(SERVER_PORT)
+	
+	multiplayer.multiplayer_peer = server_peer
+	
+	multiplayer.peer_connected.connect(_add_player_to_game)
+	multiplayer.peer_disconnected.connect(_del_player)	
+	
 func join_as_player2():
 	print("Join as player 2	")
+	
+	var client_peer = ENetMultiplayerPeer.new()
+	client_peer.create_client(SERVER_IP, SERVER_PORT)
+	
+	multiplayer.multiplayer_peer = client_peer
+
+func _add_player_to_game(id: int):
+	print("Player %s joined the game !" % id)
+
+func _del_player(id: int):
+	print("Player %s left the game !" % id)
 
